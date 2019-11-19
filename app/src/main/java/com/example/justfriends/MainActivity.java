254f5d88b2a777cd9894632a04abb17a2f84fbc6
Caroline Gilhool <carolgil@umich.edu>
2019-11-19 14:11:04 -0500

    Firebase added

--- app/src/main/java/com/example/justfriends/MainActivity.java
+++ app/src/main/java/com/example/justfriends/MainActivity.java
@@ -1,14 +1,98 @@
 package com.example.justfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonLogIn;
+    Button buttonRegister;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogIn = findViewById(R.id.buttonLogIn);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonLogIn.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonLogIn) {
+            loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        }
+
+        else if (view == buttonRegister) {
+            makeNewUsers(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        }
+    }
+
+
+    public void makeNewUsers(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // display success message
+                            Toast.makeText(MainActivity.this, "User Registration Successful", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "User Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent mainIntent = new Intent(MainActivity.this, FeedActivity.class);
+                            startActivity(mainIntent);
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
+
 }
