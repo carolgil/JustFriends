933c34805dabc3487b9c69c9e8649a731fccbf5d
sai <sadutta@umich.edu>
2019-11-21 17:04:39 -0500

    Navigation Bar Updated

--- app/src/main/java/com/example/justfriends/ProfileInfoActivity.java
+++ app/src/main/java/com/example/justfriends/ProfileInfoActivity.java
@@ -1,16 +1,46 @@
 package com.example.justfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ProfileInfoActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileInfoActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView mMainNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+
+        mMainNav.setOnNavigationItemSelectedListener(this);
+
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.home_nav) {
+            Intent landingsIntent = new Intent(ProfileInfoActivity.this, FeedActivity.class);
+            startActivity(landingsIntent);
+        } else if (menuItem.getItemId() == R.id.profile_nav) {
+
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+            Intent profileIntent = new Intent(ProfileInfoActivity.this, ProfileEventsActivity.class);
+            startActivity(profileIntent);
+        } else if (menuItem.getItemId() == R.id.chat_nav) {
+            Toast.makeText(this, "Chat", Toast.LENGTH_SHORT).show();
+            Intent chatIntent = new Intent(ProfileInfoActivity.this, ChatActivity.class);
+            startActivity(chatIntent);
+        }
+        return false;
+    }
 }
