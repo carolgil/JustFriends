6c8f11fc6c104cea05313b30ad72d461427807d3
sai <sadutta@umich.edu>
2019-11-20 21:06:32 -0500

    Navigation

--- app/src/main/java/com/example/justfriends/FeedActivity.java
+++ app/src/main/java/com/example/justfriends/FeedActivity.java
@@ -1,14 +1,39 @@
 package com.example.justfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class FeedActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FeedActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.home_nav:
+                return true;
+            case R.id.profile_nav:
+                return true;
+            case R.id.chat_nav:
+                return true;
+                default:
+                    return false;
+
+        }
     }
 }
