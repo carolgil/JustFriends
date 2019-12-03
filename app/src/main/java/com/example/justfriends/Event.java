af4789f29f644040f35bdff8f48e1255ac1ec71a
Matt Lindon <mlindon@umich.edu>
2019-12-03 13:30:51 -0500

    Created Event Class

--- app/src/main/java/com/example/justfriends/Event.java
+++ app/src/main/java/com/example/justfriends/Event.java
@@ -1,4 +1,25 @@
 package com.example.justfriends;
 
 public class Event {
+
+    public String eventName;
+    public String eventLocation;
+    public String eventDate;
+    public String eventTime;
+    public String eventDescription;
+    public int eventCap;
+
+    public Event(){
+
+    }
+
+    public Event(String eventName, String eventLocation, String eventDate, String eventTime, String eventDescription, int eventCap){
+        this.eventName = eventName;
+        this.eventLocation = eventLocation;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.eventDescription = eventDescription;
+        this.eventCap = eventCap;
+    }
+    
 }
